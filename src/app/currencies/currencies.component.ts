98cf342527a668c2d4e77ac4e0856714b1df4d0b
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-09-13 00:43:50 +0300

    currencies & style comp

--- src/app/currencies/currencies.component.ts
+++ src/app/currencies/currencies.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CurrenciesService } from './currencies.service';
 import { MatSelectChange } from '@angular/material/select';
-import { FormControl, FormGroup } from '@angular/forms';
-import { EChartsOption } from 'echarts';
+import { FormControl } from '@angular/forms';
 
 export interface Currency {
      r030: string,
@@ -22,16 +21,23 @@ export class CurrenciesComponent implements OnInit {
   start = new FormControl();
   end = new FormControl();
 
+  currency: string = '';
+  startDate: string | any = '';
+  endDate: string | any = '';
+
+  fetchedCurrencies: any = [];
+  chart: any;
+  currencyRange: any;
+
   switchTable: boolean | any;
   switchChart:  boolean | any;
-  displayedColumns: string[] = ['exchangedate', 'rate' ];
+  displayedColumns: string[] = ['exchangedate', 'rate'];
 
-  constructor(public currenciesService: CurrenciesService)  { }
+  constructor(private currenciesService: CurrenciesService)  {}
 
   ngOnInit(): void {
       this.currenciesService.getCurrenciesBeginValues()
-          .subscribe(data => this.currencies = data)
-      //this.currenciesService.getChart()
+        .subscribe(data => this.currencies = data)
   }
 
   handleSwitchTable() {
@@ -44,4 +50,87 @@ export class CurrenciesComponent implements OnInit {
     this.switchChart = true;
   }
 
+  setChoozenCurrrency({ value }: MatSelectChange) {
+    this.currency = value;
+  }
+
+  setStartDateValue({ value }: any) {
+    this.startDate = value;
+  }
+
+  setEndDateValue({ value }: any) {
+    this.endDate = value;
+  }
+
+  dateRange(startDate: any, endDate: any, steps = 1) {
+    const dateArray = [];
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= new Date(endDate)) {
+      let currentDateValue = new Date(currentDate)
+      let yyyy = currentDateValue.getFullYear().toString();
+      let mm = String(currentDateValue.getMonth() + 1).padStart(2, '0');
+      let dd = String(currentDateValue.getDate()).padStart(2, '0');
+      dateArray.push(`${yyyy}${mm}${dd}`);
+      currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+  }
+    return dateArray;
+  }
+
+  setChart() {
+    let currRange = this.dateRange(this.startDate, this.endDate);
+    this.currenciesService.handleFetchCurrenciesData(currRange, this.currency)
+    .subscribe(
+      (data: any) => (this.fetchedCurrencies = data),
+      (err: Error) => console.log(err),
+      () => this.buildChart()
+    );
+  }
+
+  setTable() {
+    let currRange = this.dateRange(this.startDate, this.endDate);
+    this.currenciesService.handleFetchCurrenciesData(currRange, this.currency)
+    .subscribe(
+      (data: any) => (this.fetchedCurrencies = data),
+      (err: Error) => console.log(err),
+      () => this.buildChart()
+    );
+  }
+
+  buildChart() {
+    let rate = this.fetchedCurrencies.map((item: any) => item.rate);
+    let exchangedate = this.fetchedCurrencies.map((item: any) => item.exchangedate);
+    this.chart = {
+      legend: {
+        data: [`UAH to ${this.currency}`],
+        align: 'left',
+      },
+      xAxis: {
+        align: 'right',
+        position: 'left',
+        type: 'category',
+        data: exchangedate,
+        splitLine: {
+          show: false,
+        },
+      },
+      yAxis: {
+        type: 'value',
+        name: 'UAN',
+        position: 'center',
+      },
+      series: [
+        {
+          label:{
+            show:true,
+            position:'bottom',
+          },
+
+          name: `UAH to ${this.currency}`,
+          data: rate,
+          type: 'line',
+        },
+      ],
+    };
+  }
 }
--- src/app/currencies/currencies.service.ts
+++ src/app/currencies/currencies.service.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Currency } from './currencies.component';
-import { Observable, range } from 'rxjs';
-import { MatSelectChange } from '@angular/material/select';
-import { concatMap, map, scan } from 'rxjs/operators';
+import { Observable, from } from 'rxjs';
+import {  map, scan, concatMap } from 'rxjs/operators';
+
 
 @Injectable()
 export class CurrenciesService {
-  currency: string = '';
-  startDate: string | any = '';
-  endDate: string | any = '';
-
-  fetchedCurrencies: any = [];
-
-  chart: any;
-
-  range: any;
 
   start_url: string =
     'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json';
@@ -28,22 +19,20 @@ export class CurrenciesService {
     return this.http.get<Currency[]>(this.start_url);
   }
 
-  getCurrenciesValue(num: number): Observable<Currency[]> {
-    let url = `${this.dateChart_url}valcode=${this.currency}&date=${String(
-      +this.startDate + num
-    )}&json`;
+  getCurrenciesValues( currency: string, date: string): Observable<Currency[]> {
+    let url = `${this.dateChart_url}valcode=${currency}&date=${date}&json`;
     return this.http.get<Currency[]>(url);
   }
 
-  handleFetchCurrencies(): Observable<Currency[]> {
-   return this.range
+  handleFetchCurrenciesData(range: Array<string>, currency: string): Observable<Currency[]> {
+   return from(range)
       .pipe(
-        concatMap((num: number) => {
-          return this.getCurrenciesValue(num);
+        concatMap(item => {
+          return this.getCurrenciesValues(currency, item);
         }),
         map(
-          (data: any[]) =>
-            data.map((item) => {
+          (data: any) =>
+            data.map((item: any) => {
               return {
                 rate: item.rate,
                 exchangedate: item.exchangedate,
@@ -54,81 +43,5 @@ export class CurrenciesService {
           return [...acc, ...item];
         }, [])
       )
-      .subscribe(
-        (data: any) => (this.fetchedCurrencies = data),
-        (err: Error) => console.log(err),
-        () => this.getChart()
-      );
-  }
-
-  setChoozenCurrrency({ value }: MatSelectChange) {
-    this.currency = value;
-  }
-
-  setStartDateValue({ value }: any) {
-    let date = new Date(value);
-    let yyyy = date.getFullYear().toString();
-    let mm = String(date.getMonth() + 1).padStart(2, '0');
-    let dd = date.getDate().toString();
-    this.startDate = `${yyyy}${mm}${dd}`;
-  }
-
-  setEndDateValue({ value }: any) {
-    let date = new Date(value);
-    let yyyy = date.getFullYear().toString();
-    let mm = String(date.getMonth() + 1).padStart(2, '0');
-    let dd = date.getDate().toString();
-    this.endDate = `${yyyy}${mm}${dd}`;
-  }
-
-  setChart() {
-    this.range = range(0, (this.endDate - this.startDate) + 1);
-    this.handleFetchCurrencies();
-  }
-
-  setTable() {
-    this.range = range(0, (this.endDate - this.startDate) + 1);
-    this.handleFetchCurrencies();
-    console.log('Table');
-  }
-
-  getChart() {
-    let rate = this.fetchedCurrencies.map((item: any) => item.rate);
-    let exchangedate = this.fetchedCurrencies.map((item: any) => item.exchangedate);
-    this.chart = {
-      legend: {
-        data: [`UAH to ${this.currency}`],
-        align: 'left',
-      },
-
-      tooltip: {},
-      xAxis: {
-
-        align: 'right',
-        position: 'left',
-        type: 'category',
-        data: exchangedate,
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {
-        type: 'value',
-        name: 'UAN',
-        position: 'center',
-      },
-      series: [
-        {
-          label:{
-            show:true,
-            position:'bottom',
-          },
-
-          name: `UAH to ${this.currency}`,
-          data: rate,
-          type: 'line',
-        },
-      ],
-    };
   }
 }
